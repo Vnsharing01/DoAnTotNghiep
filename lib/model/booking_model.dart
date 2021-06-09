7a7b3ccb2ad6130c71235a1d24e2a0ad50152854
thichu <thichu08@gmail.com>
2021-06-09 23:24:43 +0700

    create bookingModel

--- lib/model/booking_model.dart
+++ lib/model/booking_model.dart
@@ -1,3 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class BookingModel {
-  
+  BookingModel({
+    this.email,
+    this.userName,
+    this.campName,
+    this.createDate,
+    this.people,
+    this.children,
+    this.price,
+    this.stay,
+    this.checkinDate,
+    this.checkinTime,
+    this.imageUrl, 
+  });
+
+  final String email;
+  final String userName;
+  final String campName;
+  final Timestamp createDate;
+  final int people;
+  final int children;
+  final int price;
+  final String stay;
+  final String checkinDate;
+  final String checkinTime;
+  final String imageUrl;
 }
