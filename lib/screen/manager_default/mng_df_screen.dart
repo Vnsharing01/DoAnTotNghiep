f3952bbfeddc3b552c1c8d9e7ede71787587c760
thichu <thichu08@gmail.com>
2021-06-13 01:48:29 +0700

    update ui mng df screen

--- lib/screen/manager_default/mng_df_screen.dart
+++ lib/screen/manager_default/mng_df_screen.dart
@@ -7,71 +7,38 @@ class DfMngCampScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            color: Colors.white,
-            height: 64,
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    width: 36,
-                    height: 36,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    'abc@gmail.com',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: colorMediumGray,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/images/logo.png'),
+              width: 136,
+              height: 136,
             ),
-          ),
-          Expanded(
-            flex: 8,
-            child: Container(
-              height: double.infinity,
-              alignment: Alignment.center,
-              color: colorMediumGray,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image(
-                    image: AssetImage('assets/images/logo.png'),
-                    width: 136,
-                    height: 136,
-                  ),
-                  Text(
-                    'Tạo khu cắm trại của bạn trên \nYuru Camp',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  BtnItemView(
-                    press: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => MngAddCampScreen(),
-                        ),
-                      );
-                    },
-                    text: 'Thêm khu cắm trại',
-                    color: colorPrimary,
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                  ),
-                ],
-              ),
+            Text(
+              'Tạo khu cắm trại của bạn trên \nYuru Camp',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
             ),
-          ),
-        ],
+            BtnItemView(
+              press: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => MngAddCampScreen(),
+                  ),
+                );
+              },
+              text: 'Thêm khu cắm trại',
+              color: colorPrimary,
+              margin: EdgeInsets.symmetric(vertical: 10),
+            ),
+          ],
+        ),
       ),
     );
   }
